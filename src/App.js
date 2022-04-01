530a5c0eb13afe2e8ec52881fe1cd923b82479df
nzyuko <johnnzyuko514@gmail.com>
2022-04-01 11:38:16 +0300

    initial commit with some defaults react junk will remove later

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello from React</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Task Tracker</h1>
+        </div>
+    )
+}
